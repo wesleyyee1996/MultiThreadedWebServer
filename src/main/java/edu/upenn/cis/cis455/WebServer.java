3977cd8244d44b95703fd7cc6c06ba35f99153af
Zack Ives <zives@cis.upenn.edu>
2017-08-28 07:58:06 -0400

    Merge

--- src/main/java/edu/upenn/cis/cis455/WebServer.java
+++ src/main/java/edu/upenn/cis/cis455/WebServer.java
@@ -8,11 +8,14 @@ public class WebServer {
     public static void main(String[] args) {
         org.apache.logging.log4j.core.config.Configurator.setLevel("edu.upenn.cis.cis455", Level.DEBUG);
         
+        // TODO: make sure you parse *BOTH* command line arguments properly
+        
         if (args.length == 0)
             staticFileLocation("./www");
         else
             staticFileLocation(args[0]);
-            
+
+        // Register a test handler (should only work in Milestone 2)            
         get("/test", (request, result) -> "Hello 555");
         
         awaitInitialization();
