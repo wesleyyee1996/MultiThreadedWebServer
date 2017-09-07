504d5c4d003558f31c4b65bbb914adf6ae95c7cf
Zack Ives <zives@cis.upenn.edu>
2017-09-07 22:31:36 +0000

    Stub for service factory

---
+++ src/main/java/edu/upenn/cis/cis455/ServiceFactory.java
@@ -0,0 +1,58 @@
+package edu.upenn.cis.cis455;
+
+import java.net.Socket;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ServiceFactory {
+
+    /**
+     * Get the HTTP server associated with port 8080
+     */
+    public static WebService getServerInstance() {
+        return null;
+    }
+    
+    /**
+     * Create an HTTP request given an incoming socket
+     */
+    public static Request createRequest(Socket socket,
+                         String uri,
+                         boolean keepAlive,
+                         Map<String, String> headers,
+                         Map<String, List<String>> parms) {
+        return new Cis455Request(socket, uri, keepAlive, headers, parms);                         
+    }
+    
+    /**
+     * Gets a request handler for files (i.e., static content) or dynamic content
+     */
+    public static HttpRequestHandler createRequestHandlerInstance(Path serverRoot) {
+        return null;
+    }
+
+    /**
+     * Gets a new HTTP Response object
+     */
+    public static Response createResponse() {
+        return null;
+    }
+
+    /**
+     * Creates a blank session ID and registers a Session object for the request
+     */
+    public static String createSession() {
+        return null;
+    }
+    
+    /**
+     * Looks up a session by ID and updates / returns it
+     */
+    public static Session getSession(String id) {
+        
+        return null;
+    }
+}
